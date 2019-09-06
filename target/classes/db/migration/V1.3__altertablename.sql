f31049f7736a22d7c7d16058a72de35faadda121
陈俞彤 <yutong@gwu.edu>
2019-09-06 12:19:16 -0400

    Removed tsrget from the repository

--- target/classes/db/migration/V1.3__altertablename.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE area RENAME TO areas;
-ALTER TABLE hotel RENAME TO hotels;
-ALTER TABLE customer RENAME TO customers;
--- target/classes/db/migration/V1.4__altertablename.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE customers
-DROP CONSTRAINT customer_name_key;
